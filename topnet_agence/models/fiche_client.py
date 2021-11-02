6a79334272a003f11cf5895076639764faceeb50
GHARBI-R <62842493+GHARBI-R@users.noreply.github.com>
2021-11-02 21:18:47 +0100

    commit 01/11/2021

--- topnet_agence/models/fiche_client.py
+++ topnet_agence/models/fiche_client.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api,_
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 import re
 
@@ -18,9 +18,16 @@ class Clients(models.Model):
         ('email_pri_uniq', 'unique(email_pri)', 'Email existe déja'),
     ]
 
+    def action_depot(self):
+        for rec in self:
+            rec.state = 'dossier'
+
+    def action_suivi(self):
+        for rec in self:
+            rec.state = 'suivi'
 
     id_contrat = fields.Char(string='Numéro contrat', required=True, copy=False, readonly=True,
-                           index=True, default=lambda self: _('New'))
+                             index=True, default=lambda self: _('New'))
     name = fields.Char(string="Nom et Prénom du gérant")
     cin_pass = fields.Integer(string="Numéro CIN/Passeport")
     raison = fields.Char(string="Raison sociale")
@@ -32,34 +39,36 @@ class Clients(models.Model):
     correspondance = fields.Char(string="Adresse de correspondance")
     Ville = fields.Char(string="Ville")
     postale = fields.Integer(string="Code postale")
-    tel = fields.Char(string="Tél")
-    fax = fields.Char(string="Fax")
+    tel = fields.Integer(string="Tél")
+    fax = fields.Integer(string="Fax")
     installation = fields.Char(string="Adresse d'installation")
     ville2 = fields.Char(string="Ville")
     postale2 = fields.Integer(string="Code postale")
-    tel2 = fields.Char(string="Tél")
-    fax2 = fields.Char(string="Fax")
+    tel2 = fields.Integer(string="Tél")
+    fax2 = fields.Integer(string="Fax")
     #  Contact Administratif et Financiers
 
     nom = fields.Char(string="Nom et Prénom")
-    Tel_admi = fields.Char(string="Tél")
-    gsm_admi = fields.Char(string="GSM")
+    Tel_admi = fields.Integer(string="Tél")
+    gsm_admi = fields.Integer(string="GSM")
     email_admi = fields.Char(string="Email")
 
     #  Contact technique
 
     nom_tech = fields.Char(string="Nom et Prénom")
-    tel_tech = fields.Char(string="Tél")
-    gsm_tech = fields.Char(string="GSM")
+    tel_tech = fields.Integer(string="Tél")
+    gsm_tech = fields.Integer(string="GSM")
     email_tech = fields.Char(string="Email")
 
     email_pri = fields.Char(string="Email principale")
-    type_offre = fields.Selection ([('Fibre Optique','Fibre Optique'),('Voip Access','Voip Access'),('Rapido Pro','Rapido Pro')], default="Fibre Optique")
-    debit = fields.Selection ([("20","20"),("30","30"),("50","50"),("100","100")], default="20")
+    type_offre = fields.Selection(
+        [('Fibre Optique', 'Fibre Optique'), ('Voip Access', 'Voip Access'), ('Rapido Pro', 'Rapido Pro')],
+        default="Fibre Optique")
+    debit = fields.Selection([("20", "20"), ("30", "30"), ("50", "50"), ("100", "100")], default="20")
     active = fields.Boolean(string="Active", default="Ture")
     state = fields.Selection([
-        ('dossier', 'Dossier'),
-        ('suivi', 'Suivi'),
+        ('dossier', 'Dépot Dossier'),
+        ('suivi', 'Suivi Dossier'),
     ], string='Status', readonly=True, default='dossier')
 
     @api.model
@@ -69,37 +78,44 @@ class Clients(models.Model):
         result = super(Clients, self).create(vals)
         return result
 
-    # @api.constrains('name', 'cin_pass', 'tel', 'fax', 'tel2', 'fax2', 'tel_admi', 'gsm_admi', 'nom_tech', 'tel_tech', 'gsm_tech')
-    # def check_name(self):
-    #     for rec in self:
-    #         if len(str(abs(self.tel))) != 8:
-    #             raise ValidationError(_('Numéro de tel doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.cin_pass))) != 8:
-    #             raise ValidationError(_('Nméro de cin / passeport doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.fax))) != 8:
-    #             raise ValidationError(_('Nméro de fax doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.tel2))) != 8:
-    #             raise ValidationError(_('Numéro de tel doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.fax2))) != 8:
-    #             raise ValidationError(_('Nméro de fax doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.tel_admi))) != 8:
-    #             raise ValidationError(_('Nméro de tel doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.gsm_admi))) != 8:
-    #             raise ValidationError(_('Nméro de gsm doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.tel_tech))) != 8:
-    #             raise ValidationError(_('Nméro de tel doit contenir seulement 8 chiffres'))
-    #         elif len(str(abs(self.gsm_tech))) != 8:
-    #             raise ValidationError(_('Nméro de gsm doit contenir seulement 8 chiffres'))
-    #         elif len(self.name) > 20:
-    #             raise ValidationError(_('Nom trop long'))
-    #         elif len(self.nom_tech) > 20:
-    #             raise ValidationError(_('Nom trop long'))
-    #
-    # @api.constrains('email_admi', 'email_tech', 'email_pri')
-    # def validate_email(self):
-    #   for obj in self:
-    #    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$",obj.email) == None:
-    #     raise ValidationError("Vérifier votre adresse mail : %s" % obj.email)
-    #     return True
+    @api.constrains('name', 'cin_pass', 'tel', 'fax', 'tel2', 'fax2', 'Tel_admi', 'gsm_admi', 'nom_tech', 'tel_tech',
+                    'gsm_tech')
+    def check_name(self):
+        for rec in self:
+            if len(str(abs(self.tel))) != 8:
+                raise ValidationError(_('Numéro de tel doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.cin_pass))) != 8:
+                raise ValidationError(_('Nméro de cin / passeport doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.fax))) != 8:
+                raise ValidationError(_('Nméro de fax doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.tel2))) != 8:
+                raise ValidationError(_('Numéro de tel doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.fax2))) != 8:
+                raise ValidationError(_('Nméro de fax doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.Tel_admi))) != 8:
+                raise ValidationError(_('Nméro de tel administratif doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.gsm_admi))) != 8:
+                raise ValidationError(_('Nméro de gsm administratif  doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.tel_tech))) != 8:
+                raise ValidationError(_('Nméro de tel technique doit contenir seulement 8 chiffres'))
+            elif len(str(abs(self.gsm_tech))) != 8:
+                raise ValidationError(_('Nméro de gsm technique doit contenir seulement 8 chiffres'))
+            elif len(self.name) > 20:
+                raise ValidationError(_('Nom du gérant trop long'))
+            elif len(self.nom_tech) > 20:
+                raise ValidationError(_('Nom Technique trop long'))
 
+
+
+    @api.constrains('email_admi', 'email_tech', 'email_pri')
+    def validate_email(self):
+        for obj in self:
+            if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", obj.email_pri) == None:
+                raise ValidationError("Vérifier votre adresse mail principale : %s" % obj.email_pri)
+            elif re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", obj.email_admi) == None:
+                raise ValidationError("Vérifier votre adresse mail administratif: %s" % obj.email_admi)
+            elif re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", obj.email_tech) == None:
+                raise ValidationError("Vérifier votre adresse mail technique : %s" % obj.email_tech)
+
+        return True
 
