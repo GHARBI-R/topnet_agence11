29e97e2495e9ca612d78ef846eb102fd707fc3f7
GHARBI-R <62842493+GHARBI-R@users.noreply.github.com>
2021-11-12 06:06:46 +0100

    commit 01/11/2021

--- topnet_agence/models/dossier.py
+++ topnet_agence/models/dossier.py
@@ -13,6 +13,7 @@ class Topnet_dossier(models.Model):
             vals['id_dossier'] = self.env['ir.sequence'].next_by_code('topnet.dossier.sequence') or _('New')
         result = super(topnet.dossier, self).create(vals)
         return result
+
 # @api.model
 # def create(self, values):
 #     vals_dossier = {
@@ -32,6 +33,7 @@ class Topnet_dossier(models.Model):
     # dossier_id = fields.Many2one('topnet.dossier', ondelete='set null', string="dossier", index=True)
     id_dossier = fields.Char(string='Numéro Dossier', required=True, copy=False, readonly=True,
                              index=True, default=lambda self: _('New'))
+
     doc1 = fields.Image(string="Registre de commerce", max_width=90, max_height=90, verify_resolution=True, required=True)
     doc2 = fields.Image(string="CIN gérant", max_width=90, max_height=90, verify_resolution=True, required=True)
     contrat_tt = fields.Binary (string= 'telecharger contrat TT')
